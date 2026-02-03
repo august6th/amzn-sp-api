f69f10d89b04fa6b806be7a5118807b5e905244f
leeheng <heng8050lee@gmail.com>
2026-02-03 15:57:46 +0800

    v2.0.1

--- lib/SellingPartner.php
+++ lib/SellingPartner.php
@@ -12,64 +12,6 @@ use SellingPartnerApi\Contracts\Endpoint\Endpoint as EndpointContract;
 use SellingPartnerApi\Endpoint\Endpoint;
 use SellingPartnerApi\Http\Request;
 
-/**
- * @method \SellingPartnerApi\Api\AplusContent20201101\AplusContentApi aplusContent20201101Api()
- * @method \SellingPartnerApi\Api\Authorization\AuthorizationApi authorizationApi()
- * @method \SellingPartnerApi\Api\CatalogItemsV0\CatalogApi catalogV0Api()
- * @method \SellingPartnerApi\Api\CatalogItems20201201\CatalogApi catalog20201201Api()
- * @method \SellingPartnerApi\Api\CatalogItems20220401\CatalogApi catalog20220401Api()
- * @method \SellingPartnerApi\Api\DefinitionsProductTypes20200901\DefinitionsApi definitions20200901Api()
- * @method \SellingPartnerApi\Api\EasyShip20220323\EasyShipApi easyShip20220323Api()
- * @method \SellingPartnerApi\Api\FbaInbound\FbaInboundApi fbaInboundApi()
- * @method \SellingPartnerApi\Api\FbaInventory\FbaInventoryApi fbaInventoryApi()
- * @method \SellingPartnerApi\Api\FbaSmallandLight\SmallAndLightApi fbaSmallAndLightApi()
- * @method \SellingPartnerApi\Api\Feeds20200904\FeedsApi feeds20200904Api()
- * @method \SellingPartnerApi\Api\Feeds20210630\FeedsApi feeds20210630Api()
- * @method \SellingPartnerApi\Api\FinancesV0\DefaultApi financesV0Api()
- * @method \SellingPartnerApi\Api\FulfillmentInboundV0\FbaInboundApi fbaInboundV0Api()
- * @method \SellingPartnerApi\Api\FulfillmentOutbound20200701\FbaOutboundApi fbaOutbound20200701Api()
- * @method \SellingPartnerApi\Api\ListingsItems20200901\ListingsApi listings20200901Api()
- * @method \SellingPartnerApi\Api\ListingsItems20210801\ListingsApi listings20210801Api()
- * @method \SellingPartnerApi\Api\ListingsRestrictions20210801\ListingsApi listingsRestrictions20210801Api()
- * @method \SellingPartnerApi\Api\MerchantFulfillmentV0\MerchantFulfillmentApi merchantFulfillmentV0Api()
- * @method \SellingPartnerApi\Api\Messaging\MessagingApi messagingApi()
- * @method \SellingPartnerApi\Api\Notifications\NotificationsApi notificationsApi()
- * @method \SellingPartnerApi\Api\OrdersV0\OrdersV0Api ordersV0Api()
- * @method \SellingPartnerApi\Api\OrdersV0\ShipmentApi shipmentApi()
- * @method \SellingPartnerApi\Api\ProductFeesV0\FeesApi productFeesV0Api()
- * @method \SellingPartnerApi\Api\ProductPricing20220501\ProductPricingApi productPricing20220501Api()
- * @method \SellingPartnerApi\Api\ProductPricingV0\ProductPricingApi productPricingV0Api()
- * @method \SellingPartnerApi\Api\Replenishment20221107\OffersApi offers20221107Api()
- * @method \SellingPartnerApi\Api\Replenishment20221107\SellingpartnersApi sellingpartners20221107Api()
- * @method \SellingPartnerApi\Api\Reports20200904\ReportsApi reports20200904Api()
- * @method \SellingPartnerApi\Api\Reports20210630\ReportsApi reports20210630Api()
- * @method \SellingPartnerApi\Api\Sales\SalesApi salesApi()
- * @method \SellingPartnerApi\Api\Sellers\SellersApi sellersApi()
- * @method \SellingPartnerApi\Api\Services\ServiceApi serviceApi()
- * @method \SellingPartnerApi\Api\ShipmentInvoicingV0\ShipmentInvoiceApi shipmentInvoicingV0Api()
- * @method \SellingPartnerApi\Api\Shipping\ShippingApi shippingApi()
- * @method \SellingPartnerApi\Api\Solicitations\SolicitationsApi solicitationsApi()
- * @method \SellingPartnerApi\Api\Tokens20210301\TokensApi tokens20210301Api()
- * @method \SellingPartnerApi\Api\Uploads20201101\UploadsApi uploads20201101Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentInventoryV1\UpdateInventoryApi vendorDirectFulfillmentInventoryV1Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentOrders20211228\VendorOrdersApi vendorDirectFulfillmentOrders20211228Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentOrdersV1\VendorOrdersApi vendorDirectFulfillmentOrdersV1Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentPaymentsV1\VendorInvoiceApi vendorDirectInvoiceV1Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentSandboxData20211028\VendorDFSandboxApi vendorDFSandboxApi()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentSandboxData20211028\VendorDFSandboxtransactionstatusApi vendorDFSandboxtransactionstatusApi()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentShipping20211228\CustomerInvoicesApi customerInvoices20211228Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentShipping20211228\VendorShippingApi vendorShipping20211228Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentShipping20211228\VendorShippingLabelsApi vendorShippingLabels20211228Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentShippingV1\CustomerInvoicesApi customerInvoicesV1Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentShippingV1\VendorShippingApi vendorShippingV1Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentShippingV1\VendorShippingLabelsApi vendorShippingLabelsV1Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentTransactions20211228\VendorTransactionApi vendorTransaction20211228Api()
- * @method \SellingPartnerApi\Api\VendorDirectFulfillmentTransactionsV1\VendorTransactionApi vendorTransactionV1Api()
- * @method \SellingPartnerApi\Api\VendorInvoices\VendorPaymentsApi vendorPaymentsApi()
- * @method \SellingPartnerApi\Api\VendorOrders\VendorOrdersApi vendorOrdersApi()
- * @method \SellingPartnerApi\Api\VendorShipments\VendorShippingApi vendorShippingApi()
- * @method \SellingPartnerApi\Api\VendorTransactionStatus\VendorTransactionApi vendorTransactionApi()
- */
 class SellingPartner implements Signable
 {
     use SignerTrait;
@@ -274,7 +216,7 @@ class SellingPartner implements Signable
      */
     protected function createClient($arguments)
     {
-        if (isset($arguments[0]) && !empty($arguments[0])) {
+        if (!empty($arguments[0])) {
             if (!($arguments[0] instanceof ClientInterface)) {
                 throw new \InvalidArgumentException('The parameter $client must instance of GuzzleHttp\ClientInterface');
             }
@@ -291,7 +233,7 @@ class SellingPartner implements Signable
      */
     protected function createConfiguration($arguments)
     {
-        if (isset($arguments[1]) && !empty($arguments[1])) {
+        if (!empty($arguments[1])) {
             if (!($arguments[1] instanceof Configuration)) {
                 throw new \InvalidArgumentException('The parameter $configuration must instance of SellingPartnerApi\Configuration');
             }
@@ -312,7 +254,7 @@ class SellingPartner implements Signable
      */
     protected function createHeaderSelector($arguments)
     {
-        if (isset($arguments[2]) && !empty($arguments[2])) {
+        if (!empty($arguments[2])) {
             if (!($arguments[2] instanceof HeaderSelector)) {
                 throw new \InvalidArgumentException('The parameter $selector must instance of SellingPartnerApi\HeaderSelector');
             }
